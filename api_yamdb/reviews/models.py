c154a467cef835ed8a2f13935ce7a6220602df7c
CarloDiPalma <ping.06@mail.ru>
2023-04-25 11:30:46 +0300

    Custom user model

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    USER_LEVEL_CHOICES = (
+        ("admin", "Admin"),
+        ("moderator", "Moderator"),
+        ("user", "User"),
+    )
+    role = models.CharField(
+        choices=USER_LEVEL_CHOICES,
+        blank=True,
+        default="user"
+    ),
+    bio = models.TextField(
+        null=True,
+        blank=True,
+        verbose_name='Биография',
+        help_text='Биография'
+    )
