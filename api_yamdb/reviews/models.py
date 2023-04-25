5df0e84bc681eff6d105e478e435ee8fa66ddb6a
Dmitry Panteleev <flaskjr@yandex.ru>
2023-04-25 09:07:34 +0300

    Add Review model

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,7 +1,55 @@
 from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator
 from django.db import models
 
 User = get_user_model()
+
+
+class Review(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Автор отзыва',
+        help_text='Автор отзыва'
+    )
+    сomposition = models.ForeignKey(
+        Composition,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Произведение',
+        help_text='Произведение на которое написан отзыв'
+    )
+    text = models.TextField(
+        verbose_name='Текст отзыва',
+        help_text='Введи текст отзыва'
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата публикации отзыва',
+        help_text='Дата публикации отзыва'
+    )
+    rating = models.IntegerField(
+        verbose_name='Оценка произведению',
+        help_text='Введите оценку произведению',
+        # Оценка может быть целым числом от 1 до 10
+        validators=[
+            MinValueValidator(1),
+            MinValueValidator(10)
+        ],
+    )
+
+    class Meta:
+        """Делаем ограничение на количество ревью одним автором."""
+        constraints = (
+            models.UniqueConstraint(
+                fields=['author', 'composition'],
+                name=['one author = one review per composition']
+            )
+        )
+
+    def __str__(self):
+        return self.text
 
 
 class Comment(models.Model):
@@ -31,3 +79,6 @@ class Comment(models.Model):
 
     class Meta:
         ordering = ['-pub_date']
+
+    def __str__(self):
+        return self.text
