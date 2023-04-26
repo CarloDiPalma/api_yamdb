582a66fd6171ce128f8e8d7d2202829348e94a16
CarloDiPalma <ping.06@mail.ru>
2023-04-26 21:38:16 +0300

    Feature/urls (#15)
    * feature branch pr to develop test (#4)
    
    * feature branch pr to develop test
    
    * Feature PR test after review
    
    * Feature/maxim test (#5)
    
    * Testing conflict in merge
    
    * Testing conflict in merge
    
    ---------
    
    Co-authored-by: CarloDiPalma <ping.06@mail.ru>
    
    * First urls
    
    * main urls and users urls, views, serializers
    
    * serializers rename
    
    ---------
    
    Co-authored-by: Maksim-Good <120399464+Maksim-Good@users.noreply.github.com>

---
+++ api_yamdb/api/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework import routers
+
+
+# from .views import CommentViewSet
+
+# router = routers.DefaultRouter()
+# router.register('follow', FollowViewSet, basename='follow')
+
+
+urlpatterns = [
+    # path('', include(router.urls)),
+]
