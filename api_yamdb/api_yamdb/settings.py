2126e1936ef3152498deb554051dda0cb278c2f4
CarloDiPalma <ping.06@mail.ru>
2023-04-26 17:05:31 +0300

    Feature/user model version3 (#14)
    * feature branch pr to develop test (#4)
    
    * feature branch pr to develop test
    
    * Feature PR test after review
    
    * Feature/maxim test (#5)
    
    * Testing conflict in merge
    
    * Testing conflict in merge
    
    ---------
    
    Co-authored-by: CarloDiPalma <ping.06@mail.ru>
    
    * Custom user model
    
    ---------
    
    Co-authored-by: Maksim-Good <120399464+Maksim-Good@users.noreply.github.com>

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -21,8 +21,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_simplejwt',
     'api.apps.ApiConfig',
     'reviews.apps.ReviewsConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -103,3 +106,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+AUTH_USER_MODEL = 'users.User'
