a02bfa067682579ab84182b171e1c93cdb5c21ad
Свешников Максим <maksworked@ya.ru>
2023-04-27 21:58:20 +0300

    delete comments

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -17,29 +17,9 @@ class GenreSerializer(serializers.ModelSerializer):
 
 
 class TitleSerializer(serializers.ModelSerializer):
-    # def __init__(self, *args, **kwargs):
-    #     super(TitleSerializer, self).__init__(*args, **kwargs)
-    #     if 'view' in self.context and self.context['view'].action != 'create':
-    #         self.fields.update({"genre": GenreSerializer(many=True, required=False)})
-    #         self.fields.update({"category": CategorySerializer(many=False, required=False)})
     genre = GenreSerializer(many=True, )
     category = CategorySerializer(many=False, required=False)
 
     class Meta:
         fields = ('id', 'name', 'year', 'description', 'genre', 'category')
         model = Title
-
-    # def create(self, validated_data):
-    #     genres = validated_data.pop('genre')
-    #     category = validated_data.pop('category')
-    #     title = Title.objects.create(**validated_data)
-    #     for genre in genres:
-    #         current_genre = Genre.objects.get(slug=genre)
-    #         title.genre.add(current_genre)
-    #         title.save()
-    #     current_category = Category.objects.get(slug=category)
-    #     title.category = current_category
-    #     title.save()
-    #     return title
-
-
