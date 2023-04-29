3745abe4696a5e59010d0c4129bced559b366e2b
CarloDiPalma <ping.06@mail.ru>
2023-04-29 13:47:34 +0300

    Feature/users registration (#25)
    * feature branch pr to develop test (#4)
    
    * feature branch pr to develop test
    
    * Feature PR test after review
    
    * Feature/maxim test (#5)
    
    * Testing conflict in merge
    
    * Testing conflict in merge
    
    ---------
    
    Co-authored-by: CarloDiPalma <ping.06@mail.ru>
    
    * users, registration tests done
    
    ---------
    
    Co-authored-by: Maksim-Good <120399464+Maksim-Good@users.noreply.github.com>

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,7 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.shortcuts import get_object_or_404
 from rest_framework import serializers
+
 from reviews.models import Category, Comment, Genre, Review, Title
 
 
