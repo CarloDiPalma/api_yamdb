3da0152d24c4a3726643a754a5c45012ab780b05
CarloDiPalma <ping.06@mail.ru>
2023-04-29 18:37:56 +0300

    flake8, isort

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -52,28 +52,15 @@ class CommentPermissions(BasePermission):
             return True
         user = request.user
         if not user.is_anonymous:
-            if request.method == 'PATCH' and user.role == 'user' and obj.author_id != user.id:
+            if request.method == 'PATCH' and user.role == 'user' \
+                    and obj.author_id != user.id:
                 return False
-            if request.method == 'DELETE' and user.role == 'user' and obj.author_id == user.id:
+            if request.method == 'DELETE' and user.role == 'user' \
+                    and obj.author_id == user.id:
                 return True
-            if request.method == 'DELETE' and user.role == 'user' and obj.author_id != user.id:
+            if request.method == 'DELETE' and user.role == 'user' \
+                    and obj.author_id != user.id:
                 return False
         return (user.is_authenticated
                 and (obj.author == user or user.is_admin or user.is_moderator)
                 )
-
-
-class CategoryPermissions(BasePermission):
-    def has_permission(self, request, view):
-        user = request.user
-        return (
-            request.method in SAFE_METHODS
-            or user.is_authenticated and user.is_admin
-        )
-
-    def has_object_permission(self, request, view, obj):
-        user = request.user
-        return (
-            request.method in SAFE_METHODS
-            or user.is_authenticated and user.is_admin
-        )
